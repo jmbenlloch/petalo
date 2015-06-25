7a947161b44d56c3b8da288a434a101fba3f85b5
jmbenlloch <info@jmbenlloch.net>
2015-06-25 19:03:12 +0200

    base project

---
+++ petAnalysis/petAnalysis.cc
@@ -0,0 +1,68 @@
+
+#include<petAnalysis.h>
+
+ClassImp(petAnalysis)
+
+//==========================================================================
+petAnalysis::petAnalysis(gate::VLEVEL vl, std::string label) : 
+IAlgo(vl,"petAnalysis",0,label){
+//==========================================================================
+
+
+}
+
+//==========================================================================
+petAnalysis::petAnalysis(const gate::ParamStore& gs, 
+			   gate::VLEVEL vl, std::string label) :
+  IAlgo(gs,vl,"petAnalysis",0,label){
+//==========================================================================
+
+
+}
+
+//==========================================================================
+bool petAnalysis::initialize(){
+//==========================================================================
+
+  _m.message("Intializing algorithm",this->getAlgoLabel(),gate::NORMAL);
+  
+  gate::Centella::instance()
+    ->hman()->h1(this->alabel("Energy"),"Energy SiPM",10000,0,10000);
+
+  return true;
+
+}
+
+//==========================================================================
+bool petAnalysis::execute(gate::Event& evt){
+//==========================================================================
+
+  _m.message("Executing algorithm",this->getAlgoLabel(),gate::VERBOSE);
+  
+  _m.message("Event number:",evt.GetEventID(),gate::VERBOSE);
+
+  int energy = 0;
+  for(unsigned int i=0;i<evt.GetMCSensHits().size(); i++){
+	  energy += evt.GetMCSensHits()[i]->GetAmplitude();
+  }
+  
+  gate::Centella::instance()
+    ->hman()->fill(this->alabel("Energy"),energy);
+  
+  return true;
+
+}
+
+//==========================================================================
+bool petAnalysis::finalize(){
+//==========================================================================
+
+  _m.message("Finalising algorithm",this->getAlgoLabel(),gate::NORMAL);
+
+  gate::Run* run = &gate::Centella::instance()->getRun();
+  int nevt = gate::int_from_string(run->fetch_sstore("num_events"));
+  _m.message("Number of generated events in file:",nevt,gate::NORMAL);
+  
+  return true;
+
+}
---
+++ petAnalysis/petAnalysis.h
@@ -0,0 +1,37 @@
+#ifndef _petAnalysis__
+#define _petAnalysis__
+
+#include <GATE/Centella.h>
+
+class petAnalysis : public gate::IAlgo {
+
+ public:
+  
+  //! default contructor
+  petAnalysis(gate::VLEVEL=gate::NORMAL,
+	       std::string label="petAnalysisInstance");
+  
+  //! constructor with store with input parameters 
+  petAnalysis(const gate::ParamStore& gs,
+	       gate::VLEVEL=gate::NORMAL,
+	       std::string label="petAnalysisInstance");
+  
+  //! destructor
+  virtual ~petAnalysis(){};
+  
+  //! initialize algorithm
+  bool initialize();        
+  
+  //! execute algorithm: process current event
+  bool execute(gate::Event& evt);  
+  
+  //! finalize algorithm
+  bool finalize();          
+  
+ private:
+  
+  ClassDef(petAnalysis,0)
+    
+};
+
+#endif
