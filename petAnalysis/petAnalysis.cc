67e5a1cd71d171d0f13336dc95cbcf64468c8ee8
jmbenlloch <info@jmbenlloch.net>
2015-06-28 18:07:58 +0200

    count photoelectric and compton events

--- petAnalysis/petAnalysis.cc
+++ petAnalysis/petAnalysis.cc
@@ -30,6 +30,13 @@ bool petAnalysis::initialize(){
   gate::Centella::instance()
     ->hman()->h1(this->alabel("Energy"),"Energy SiPM",10000,0,10000);
 
+  //gate::Run* run = &gate::Centella::instance()->getRun();
+  //int nevt = gate::int_from_string(run->fetch_sstore("num_events"));
+  //_m.message("Number of generated events in file:",nevt,gate::NORMAL);
+
+  store("photoCount",0);
+  store("photoCompton",0);
+
   return true;
 
 }
@@ -50,10 +57,56 @@ bool petAnalysis::execute(gate::Event& evt){
   gate::Centella::instance()
     ->hman()->fill(this->alabel("Energy"),energy);
 
-  //find primary particle:
-//  for(unsigned int i=0;i<evt.GetMCParticles().size();i++){
-//	  std::cout << "Primary: " << evt.GetMCParticles()[i]->IsPrimary() << "; Process: " << evt.GetMCParticles()[i]->GetCreatorProc() << " - Daughters: " << evt.GetMCParticles()[i]->GetDaughters().size() << std::endl;
- // }
+  int countPhoto = fetch_istore("photoCount");
+  int countCompton = fetch_istore("photoCompton");
+  int comptFlag = 0;
+
+  gate::MCParticle* primary;
+
+  //std::cout << "Particles: "<< evt.GetMCParticles().size() << "\n";
+  for(unsigned int i=0;i<evt.GetMCParticles().size();i++){
+	  gate::MCParticle* particle = evt.GetMCParticles()[i];
+	  if(particle->IsPrimary()){
+
+		  primary = particle;
+
+//		  std::cout << "Primary daughters: " << particle->GetDaughters().size();
+		  //std::vector<gate::MCParticle*> daughters = particle->GetDaughters();
+		  for(unsigned int j=0; j<particle->GetDaughters().size();j++){
+			  if(particle->GetDaughters()[j]->GetCreatorProc() == std::string("compt")){
+				 comptFlag = 1;
+				 break;
+			  }
+		  }
+	  }
+  }
+
+  if(comptFlag){
+	  countCompton++;
+	  //std::cout << "\t Compton\n";
+	  //Need to find first interaction
+	  //for(unsigned int j=0; j<primary->GetDaughters().size();j++){
+		//  std::cout << "\t Vol: " <<primary->GetDaughters()[j]->GetInitialVol();
+	  //}
+	  //std::cout << std::endl;
+  }else{
+	  countPhoto++;
+	  /*std::cout << "\t Photo ->";
+	  for(unsigned int j=0; j<primary->GetDaughters().size();j++){
+		  std::cout << "\t PDG/Proc/Energy: " << primary->GetDaughters()[j]->GetPDG() << "/" << primary->GetDaughters()[j]->GetCreatorProc() << "/" << primary->GetDaughters()[j]->GetInitialMom().GetE();
+	  } 
+	  std::cout << std::endl;
+	  //If only one particle generated then should be energy deposition (due to geant4 IR cut)
+	  if(primary->GetDaughters().size() == 1){
+		  //Only one track
+		  std::cout << "Tracks: " << primary->GetDaughters()[0]->GetTracks().size();
+		  std::cout << "\t Hits: " << primary->GetDaughters()[0]->GetTracks()[0]->GetHits().size();
+		  std::cout << "\t HitsEnergy: " << primary->GetDaughters()[0]->GetTracks()[0]->GetHitsEnergy() << std::endl;
+	  }*/
+  }
+
+  fstore("photoCount",countPhoto);
+  fstore("photoCompton",countCompton);
 
   return true;
 
@@ -69,10 +122,13 @@ bool petAnalysis::finalize(){
   int nevt = gate::int_from_string(run->fetch_sstore("num_events"));
   _m.message("Number of generated events in file:",nevt,gate::NORMAL);
 
+  std::cout << "Events with photoelectric first: " << fetch_istore("photoCount") << std::endl;
+  std::cout << "Events with compton first: " << fetch_istore("photoCompton") << std::endl;
+
   // Actual hist name includes algorithm's name
   TH1* hist = gate::Centella::instance()->hman()->operator[]("petAnalysis_Energy");
   hist->Rebin(125);
-  double maxV = hist->GetBinCenter( hist->GetMaximumBin() );
+  //double maxV = hist->GetBinCenter( hist->GetMaximumBin() );
   TF1* gauF = new TF1("gauF","gaus",0,10000);
   hist->Fit("gauF","","e",5000,6000);
   std::cout << "FWHM res = " << 2.35*gauF->GetParameter(2)/gauF->GetParameter(1) << std::endl;
