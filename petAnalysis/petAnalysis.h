4b724a57d8a8ef1b9cae33c60ec49c9c69cd5bfb
jmbenlloch <info@jmbenlloch.net>
2015-06-30 14:03:14 +0200

    some code refactoring...

--- petAnalysis/petAnalysis.h
+++ petAnalysis/petAnalysis.h
@@ -25,9 +25,13 @@ class petAnalysis : public gate::IAlgo {
   //! execute algorithm: process current event
   bool execute(gate::Event& evt);  
   
+  //Position reconstruction using barycenter
+  void reconstruction(gate::Event& evt, gate::Point3D& pt);
   //Fill energy histogram
   void energyHist(gate::Event& evt);
-
+  //Find first particle in a vector of particles by its creation time
+  void findFirstParticle(const std::vector<const gate::MCParticle*> particles, gate::MCParticle& first);
+  void findFirstParticle(std::vector<gate::MCParticle*> particles, gate::MCParticle& first);
 
   //! finalize algorithm
   bool finalize();          
